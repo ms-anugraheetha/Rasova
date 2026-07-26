b75872d19c81b70b59b3f24c699e28224f9351ce
Anugraheetha Bin Saj <anugraheetha.bs@gmail.com>
2026-07-26 20:22:21 +0300

    Set up Authentication and frontend tooling

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
-use App\Http\Controllers\RazorpayWebhookController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RazorpayWebhookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::post('/webhooks/razorpay', [RazorpayWebhookController::class, 'handle']);
+
+require __DIR__.'/auth.php';
